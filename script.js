a8b103ff72098f7204a6cbfe95e4fa66eb3e4267
MSebb23 <matteosabbatini23@gmail.com>
2021-03-30 09:30:26 +0200

    print contact with v-for

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ var vue_box = new Vue (
                     name: 'Michele',
                     avatar: '_1',
                     visible: true,
-                    messages: [{
+                    messages: [
+                        {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
-                    },
+                        },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di dargli da mangiare',
@@ -28,11 +29,12 @@ var vue_box = new Vue (
                     name: 'Fabio',
                     avatar: '_2',
                     visible: true,
-                    messages: [{
+                    messages: [
+                        {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
-                    },
+                        },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
@@ -49,11 +51,12 @@ var vue_box = new Vue (
                     name: 'Samuele',
                     avatar: '_3',
                     visible: true,
-                    messages: [{
+                    messages: [
+                        {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
-                    },
+                        },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
@@ -70,11 +73,12 @@ var vue_box = new Vue (
                     name: 'Luisa',
                     avatar: '_4',
                     visible: true,
-                    messages: [{
+                    messages: [
+                        {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
-                    },
+                        },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
